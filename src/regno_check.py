3271ae6f6432938dc36cf5e3d8ddcfdebc8a4413
morisu24 <moritaro24.105@gmail.com>
2021-11-08 11:15:39 +0900

    一応完成

--- src/regno_check.py
+++ src/regno_check.py
@@ -17,6 +17,7 @@ class GUI(threading.Thread):
   def __init__(self):
     threading.Thread.__init__(self)
     self.value = 0
+    self.quit_flag = False
   
   def start(self):
     self.root = tk.Tk()
@@ -36,24 +37,22 @@ class GUI(threading.Thread):
     self.label_before = tk.Label(self.root, textvariable=self.sv_before, fg="#c5c5c5", font=("Arial", "30", "bold"))
     self.label_before.pack()
 
-    self.label_new = tk.Label(self.root, textvariable=self.sv_new, font=("Arial", "30", "bold"))
+    self.label_new = tk.Label(self.root, textvariable=self.sv_new, font=("Arial", "40", "bold"))
     self.label_new.pack()
 
     # ボタンの表示
-    self.button = tk.Button(self.root, text='Click to Quit', command=self.quit_gui)
+    self.button = tk.Button(self.root, text='Save & Quit', command=self.quit_gui)
     self.button.pack()
     self.check_regno_callback()
     self.root.mainloop()
 
   # 別スレッドで実行するコールバック
   def check_regno_callback(self):
-    th = threading.Thread(target=self.check_regno, args=())
+    th = threading.Thread(target=self.check_regno, args=(), daemon=True)
     th.start()
 
   def on_connect_nfc(self, tag):
-    # タグのIDなどを出力する
-    # print tag
-    
+    # nfcタグ読み取り  
     if isinstance(tag, nfc.tag.tt3.Type3Tag):
       try:
           sc = nfc.tag.tt3.ServiceCode(service_code >> 6 ,service_code & 0x3f)
@@ -61,7 +60,8 @@ class GUI(threading.Thread):
           data = tag.read_without_encryption([sc],[bc])
           sid = data[2:12]
           regno = sid.decode()
-          # print (sid)
+          
+          print (regno)
           self.regno_list.append(regno)
           self.sv_before.set(self.regno_list[-2])
           self.sv_new.set(self.regno_list[-1])
@@ -74,29 +74,30 @@ class GUI(threading.Thread):
   # 
   def check_regno(self):
     clf = nfc.ContactlessFrontend('usb')
-    while True:
+    while not(self.quit_flag):
       clf.connect(rdwr={'on-connect': self.on_connect_nfc})
-      time.sleep(1)
+      time.sleep(2)  # 読み取り間隔
     
   
   def quit_gui(self):
-    print("quit")
+    self.quit_flag = True
     self.root.destroy()
 
 
 def save_csv(regno_list):
   now = datetime.datetime.now()
   filename = '../output/log_' + now.strftime('%Y%m%d_%H%M%S') + '.csv'
-  str_regno = '\n'.join(regno_list)
+  str_regno = '\n'.join(regno_list[2:])
   with open(filename, 'w') as f:
     writer = csv.writer(f, lineterminator='\n')
     f.write(str_regno)
-    
+  print ("saved csv")
 
 def main():
   gui = GUI()
   gui.start()
   save_csv(gui.regno_list)
+  sys.exit()
   
 
 if __name__ == '__main__':
